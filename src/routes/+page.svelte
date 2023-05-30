afa26918c34d118c47eaf9c50227d5e4117458dd
runnyoats <ammarnsyll@gmail.com>
2023-05-30 08:00:45 +0800

    Navbar & Dashboard (Temp)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <h1 class="text-3xl font-bold underline">
-  Hello world! test
+  Hello world!
 </h1>
 
 <style lang="postcss">

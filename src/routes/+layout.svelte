8c945f6abeaf099717e5b6c06c4bfc66e9fa278b
runnyoats <ammarnsyll@gmail.com>
2023-05-30 09:42:43 +0800

    Prettier + Cleaned layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,75 +1,34 @@
 <script>
-	import '../app.css';
 	import { page } from '$app/stores';
+	import '../app.css';
 	const routes = [
-		// { href: '/', name: 'Todo App', tooltip: 'Todo' },
-		{ href: '/dashboard', name: 'Dashboard', tooltip: 'Dashboard' },
-		{ href: '/sync', name: 'Sync Data', tooltip: 'Sync Data' },
-		{ href: '/murid', name: 'Murid', tooltip: 'Murid' },
-		{ href: '/logout', name: 'Log Out', tooltip: 'Log Out' },
+		{ href: '/dashboard', name: 'Dashboard' },
+		{ href: '/sync', name: 'Sync Data' },
+		{ href: '/murid', name: 'Murid' },
+		{ href: '/logout', name: 'Log Out' }
 	];
-
 </script>
 
-<div class="drawer drawer-mobile text-sm">
-	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-	<div class="drawer-content flex flex-col items-center justify-center relative">
-		<!-- Page content here -->
-		<label for="my-drawer-2" class="drawer-button btn btn-square lg:hidden absolute inset-2">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="w-6 h-6"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-				/>
-			</svg>
-		</label>
-		<div class="flex flex-col w-full h-full items-center justify-center">
-			<div class="navbar bg-base-200 flex">
-				<div class="flex-1 ml-14 lg:m-0 font-semibold gap-2 text-xl text-base-content">
-					Mental Health Dashboard
-				</div>
-				<div class="flex-none gap-2">
-					<div class="dropdown dropdown-end">
-						<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-						<!-- svelte-ignore a11y-label-has-associated-control -->
-						<label tabindex="0" class="btn btn-ghost btn-circle avatar">
-						</label>
-						<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-						<ul
-							tabindex="0"
-							class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-300 rounded-box w-52"
-						>
-							<li>
-								<a href="/about"> About </a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="flex w-full h-full items-center justify-center overflow-y-hidden"><slot /></div>
-		</div>
-	</div>
-	<div class="drawer-side">
-		<label for="my-drawer-2" class="drawer-overlay" />
-		<ul class="menu p-4 w-72 bg-base-200 text-base-content">
-			<!-- Sidebar content here -->
-			<div class="mt-12 space-y-2">
+<div class="flex h-screen bg-gray-200">
+	<div class="p-5 bg-white w-64 space-y-4">
+		<h1 class="text-2xl font-bold text-gray-700">Mental Health Dashboard</h1>
+		<nav>
+			<ul>
 				{#each routes as route}
-					<li>
-						<a href={route.href} class:active={$page.url.pathname === route.href}>
+					<li class="py-1">
+						<a
+							href={route.href}
+							class:active={$page.url.pathname === route.href}
+							class="text-gray-700 hover:text-blue-500 transition duration-200"
+						>
 							{route.name}
 						</a>
 					</li>
 				{/each}
-			</div>
-		</ul>
+			</ul>
+		</nav>
 	</div>
+	<main class="flex-1 p-5 overflow-auto">
+		<slot />
+	</main>
 </div>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,24 +1,23 @@
 <script>
-    // You can put any JavaScript code here
-  </script>
-  
-  <style>
-    .container {
-      width: 90%;
-      max-width: 930px;
-    }
-  </style>
-  
-  <div class="container">
-    <h1>MENDA Dashboard</h1>
-    <p>This is the Mental Health Dashboard for SMK Datuk Hj. Mohamed Nor Ahmad.</p>
-    <iframe 
-        title="menda-dashboard-draft-7" 
-        width="100%" 
-        height="541.25" 
-        src="https://app.powerbi.com/reportEmbed?reportId=ec7675a9-76b7-48b5-b805-54ac051b2d8c&autoAuth=true&ctid=0ad0fbf8-69ca-4e5e-afad-cd70424ac626" 
-        frameborder="0" 
-        allowFullScreen="true">
-    </iframe>
-  </div>
-  
+	// You can put any JavaScript code here
+</script>
+
+<div class="container">
+	<h1>MENDA Dashboard</h1>
+	<p>This is the Mental Health Dashboard for SMK Datuk Hj. Mohamed Nor Ahmad.</p>
+	<iframe
+		title="menda-dashboard-draft-7"
+		width="100%"
+		height="541.25"
+		src="https://app.powerbi.com/reportEmbed?reportId=ec7675a9-76b7-48b5-b805-54ac051b2d8c&autoAuth=true&ctid=0ad0fbf8-69ca-4e5e-afad-cd70424ac626"
+		frameborder="0"
+		allowFullScreen="true"
+	/>
+</div>
+
+<style>
+	.container {
+		width: 90%;
+		max-width: 930px;
+	}
+</style>

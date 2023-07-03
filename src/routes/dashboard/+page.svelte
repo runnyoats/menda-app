be7d139a1154287ff0cf83c706820a5850a1b559
runnyoats <ammarnsyll@gmail.com>
2023-07-03 18:41:09 +0800

    clean dashboard page

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,19 +1,15 @@
-<script>
-	// You can put any JavaScript code here
-</script>
+<h1 class="font-display font-black text-4xl text-white text-center md:text-left md:mr-4">
+	Power BI Dashboard
+</h1>
 
-<div class="min-h-screen bg-gray-100 flex items-center justify-center">
-	<div class="bg-white p-10 rounded-lg shadow-md w-full h-full max-w-screen-lg">
-		<h2 class="text-2xl font-bold mb-10 text-gray-800 text-center">MENDA Dashboard</h2>
-		<div class="relative w-full h-0 overflow-hidden" style="padding-bottom: 80%;">
-			<iframe
-				title="menda-dashboard-draft-DB2-3"
-				width="1140"
-				height="541.25"
-				src="https://app.powerbi.com/reportEmbed?reportId=031904be-d4aa-4037-bc6d-e7680756bc57&autoAuth=true&ctid=0ad0fbf8-69ca-4e5e-afad-cd70424ac626"
-				frameborder="0"
-				allowFullScreen="true"
-			/>
-		</div>
+<div class="rounded-xl p-4 border border-gray-400 bg-white">
+	<div class="relative w-full h-0 overflow-hidden" style="padding-bottom: 50%;">
+		<iframe
+			class="absolute top-0 left-0 w-full h-full"
+			title="menda-dashboard-draft-DB2-3"
+			src="https://app.powerbi.com/reportEmbed?reportId=031904be-d4aa-4037-bc6d-e7680756bc57&autoAuth=true&ctid=0ad0fbf8-69ca-4e5e-afad-cd70424ac626"
+			frameborder="0"
+			allowFullScreen="true"
+		/>
 	</div>
 </div>

819bf92694730c6bebdcf25456ac16ef5cd235e6
runnyoats <ammarnsyll@gmail.com>
2023-07-08 00:20:04 +0800

    better scraper code

--- src/webscraper/sepkm_scraper.py
+++ src/webscraper/sepkm_scraper.py
@@ -28,21 +28,20 @@ def extract_students(session):
     row = []
     mod = 0
 
-    # Testing purposes
-
-    """ tahun = [
+    tahun = [
         "TINGKATAN SATU",
         "TINGKATAN DUA",
         "TINGKATAN TIGA",
         "TINGKATAN EMPAT",
         "TINGKATAN LIMA",
     ]
-    kelas = ["AKIK", "BAIDURI", "DELIMA", "JED", "NILAM"] """
+    kelas = ["AKIK", "BAIDURI", "DELIMA", "JED", "NILAM"]
 
-    tahun = [
-        "TINGKATAN SATU",
-    ]
-    kelas = ["AKIK", "BAIDURI"]
+    # Testing purposes
+    # tahun = [
+    #     "TINGKATAN SATU",
+    # ]
+    # kelas = ["AKIK", "BAIDURI"]
 
     for p in tahun:
         params_class["tingkatan"] = p
@@ -73,7 +72,7 @@ def extract_students(session):
     ]
     df_stud = pd.DataFrame(table, columns=row_headers)
     df_stud.to_csv("student_data.csv")
-    return f"Produced student_data.csv."
+    return df_stud
 
 
 # Function to extract table data for PHQ9, GAD7 & SEMAK
@@ -108,8 +107,8 @@ def extract_phq9(session):
     params_page = {"page": ""}
 
     # Testing purposes
-    # page = [str(i) for i in range(1, 1001)]
-    page = [str(i) for i in range(1, 3)]
+    # page = [str(i) for i in range(1, 3)]
+    page = [str(i) for i in range(1, 1001)]
 
     table = []
     for p in page:
@@ -156,7 +155,7 @@ def extract_phq9(session):
 
     # Convert df to csv
     df_phq.to_csv("phq9_data.csv")
-    return f"Produced phq9_data.csv."
+    return df_phq
 
 
 def extract_gad7(session):
@@ -164,8 +163,8 @@ def extract_gad7(session):
     params_page = {"page": ""}
 
     # Testing purposes
-    # page = [str(i) for i in range(1, 1001)]
-    page = [str(i) for i in range(1, 3)]
+    # page = [str(i) for i in range(1, 3)]
+    page = [str(i) for i in range(1, 1001)]
 
     table = []
     for p in page:
@@ -210,7 +209,7 @@ def extract_gad7(session):
 
     # Convert df to csv
     df_gad.to_csv("gad7_data.csv")
-    return f"Produced gad7_data.csv."
+    return df_gad
 
 
 def extract_semak(session):
@@ -218,8 +217,8 @@ def extract_semak(session):
     params_page = {"page": ""}
 
     # Testing purposes
-    # page = [str(i) for i in range(1, 1001)]
-    page = [str(i) for i in range(1, 3)]
+    # page = [str(i) for i in range(1, 3)]
+    page = [str(i) for i in range(1, 1001)]
 
     table = []
     for p in page:
@@ -279,7 +278,105 @@ def extract_semak(session):
 
     # Convert df to csv
     df_semak.to_csv("semak_data.csv")
-    return f"Produced semak_data.csv."
+    return df_semak
+
+
+def process_csv_files(year, df_gad, df_phq, df_semak, df_stud):
+    # Unneeded column
+    df_stud = df_stud.drop(
+        ["bil", "sijil_lahir", "alamat", "tingkatan", "kelas"], axis=1
+    )
+
+    # Create 'jantina' column based on 'kp' values
+    df_stud["jantina"] = df_stud["kp"].apply(
+        lambda x: "L" if int(str(x)[-1]) % 2 == 1 else "P"
+    )
+
+    # Change date format
+    for date in df_stud["tarikh_lahir"]:
+        try:
+            pd.to_datetime(date, format="%d-%m-%Y")
+        except ValueError:
+            print(f"Cannot convert {date}")
+
+    df_stud.to_csv("df_stud.csv")
+
+    # Merge dataframes based on 'nama' column
+    merged_df = (
+        df_stud.merge(df_semak, on="nama")
+        .merge(df_phq, on="nama")
+        .merge(df_gad, on="nama")
+    ).copy()
+
+    merged_df.to_csv("submissions-part-0.5.csv")
+
+    # Drop duplicate class columns
+    merged_df = merged_df.drop(
+        ["nama", "tarikh_lahir", "jantina", "kelas_x", "kelas_y"], axis=1
+    ).copy()
+
+    merged_df.to_csv("submissions-part-1.csv")
+
+    # Add 'sidang' column (according to year data chosen to extract from SePKM)
+    merged_df["sidang"] = year
+
+    # Convert date columns to datetime
+    merged_df["phq_tarikh"] = pd.to_datetime(merged_df["phq_tarikh"], format="%d-%m-%Y")
+    merged_df["gad_tarikh"] = pd.to_datetime(merged_df["gad_tarikh"], format="%d-%m-%Y")
+    merged_df["semak_tarikh"] = pd.to_datetime(
+        merged_df["semak_tarikh"], format="%d-%m-%Y"
+    )
+
+    # Convert date format to yyyy-mm-dd
+    merged_df["phq_tarikh"] = merged_df["phq_tarikh"].dt.strftime("%Y-%m-%d")
+    merged_df["gad_tarikh"] = merged_df["gad_tarikh"].dt.strftime("%Y-%m-%d")
+    merged_df["semak_tarikh"] = merged_df["semak_tarikh"].dt.strftime("%Y-%m-%d")
+
+    # Extract 'tingkatan' from 'kelas'
+    merged_df["tingkatan"] = merged_df["kelas"].str.extract(r"T(\d+)-")
+    merged_df["tingkatan"] = merged_df["tingkatan"].astype(int)
+
+    # Before the problematic line
+    merged_df.to_csv("submissions-part-2.csv")
+
+    # Modify the problematic line to handle potential issues
+    try:
+        merged_df["kelas"] = merged_df["kelas"].str.split("-", expand=True)[1]
+    except KeyError:
+        print("Could not split 'kelas' or column '1' does not exist after split.")
+
+    # Convert 'merged_df' to the desired column order
+    desired_order = ["kp", "sidang", "tingkatan", "kelas"]
+    phq_columns = [
+        col
+        for col in merged_df.columns
+        if col.startswith("phq_") and col != "phq_tarikh"
+    ]
+    gad_columns = [
+        col
+        for col in merged_df.columns
+        if col.startswith("gad_") and col != "gad_tarikh"
+    ]
+    semak_columns = [
+        col
+        for col in merged_df.columns
+        if col.startswith("semak_") and col != "semak_tarikh"
+    ]
+
+    new_columns = (
+        desired_order
+        + phq_columns
+        + ["phq_tarikh"]
+        + gad_columns
+        + ["gad_tarikh"]
+        + semak_columns
+        + ["semak_tarikh"]
+    )
+
+    merged_df = merged_df[new_columns]
+
+    df_stud.to_csv("students.csv")
+    merged_df.to_csv("submissions.csv")
 
 
 def sepkm_scraper(username, password, year):
@@ -291,19 +388,17 @@ def sepkm_scraper(username, password, year):
     if not change_year(session, year):
         return {"error": "Year change failed"}, 400
 
-    student_data_message = extract_students(session)
-    phq9_data_message = extract_phq9(session)
-    gad7_data_message = extract_gad7(session)
-    semak_data_message = extract_semak(session)
+    df_stud = extract_students(session)
+    df_phq9 = extract_phq9(session)
+    df_gad7 = extract_gad7(session)
+    df_semak = extract_semak(session)
+
+    process_csv_files(year, df_gad7, df_phq9, df_semak, df_stud)
 
     return {
         "username": username,
         "password": password,
         "year": year,
-        "message_student_data": student_data_message,
-        "message_phq9_data": phq9_data_message,
-        "message_gad7_data": gad7_data_message,
-        "message_semak_data": semak_data_message,
     }
 
 
